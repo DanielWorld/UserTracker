2288d27085c780130737f6dd946afe17ca4edace
danielworld <op7773hons@gmail.com>
2015-10-26 23:19:08 +0900

    Change json tag name to fit into MySQL database columns.

--- UserTracker/src/main/java/com/danielworld/usertracker/variables/TrackingMapKey.java
+++ UserTracker/src/main/java/com/danielworld/usertracker/variables/TrackingMapKey.java
@@ -10,23 +10,22 @@ package com.danielworld.usertracker.variables;
  */
 public class TrackingMapKey {
 
-    public static final String TRACKING_APP_ID = "tracking_app_id";
-    public static final String TRACKING_TAG = "tracking_tag";
+    public static final String TRACKING_APP_ID = "app_id";
+    public static final String TRACKING_TAG = "tag";
 
-    public static final String TRACKING_EVENT = "tracking_event";
-    public static final String TRACKING_TYPE = "tracking_type";
-    public static final String TRACKING_OCCUR_TIME = "tracking_occur_time";
+    public static final String TRACKING_EVENT = "tracking_type";
+    public static final String TRACKING_OCCUR_TIME = "tracking_time";
 
     public static final String INSTALL_REFERRER = "install_referrer";
     public static final String GOOGLE_ACCOUNT = "hashed_google_account";
     public static final String DEVICE_ID = "device_id";
     public static final String GOOGLE_AD_ID = "google_ad_id";
-    public static final String OS_VERSION = "os_ver";
-    public static final String APP_PACKAGE_NAME = "app_package_name";
-    public static final String APP_VERSION_CODE = "app_ver_code";
-    public static final String APP_VERSION_NAME = "app_ver_name";
+    public static final String OS_VERSION = "os_version";
+    public static final String APP_PACKAGE_NAME = "package_name";
+    public static final String APP_VERSION_CODE = "version_code";
+    public static final String APP_VERSION_NAME = "version_name";
     public static final String DEVICE_NAME = "device_name";
-    public static final String DEVICE_MANUFACTURE = "device_manufacture";
+    public static final String DEVICE_MANUFACTURE = "manufacture";
     public static final String COUNTRY = "country";
     public static final String LANGUAGE = "language";
     public static final String NETWORK_PROVIDER = "network_provider";
